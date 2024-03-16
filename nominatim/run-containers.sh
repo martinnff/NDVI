5942943df6bab0db5b06a39a8c1345553f4cdc57
martin <mar1fer1per@gmail.com>
2024-03-16 15:17:24 +0100

    Real data examples

--- nominatim/run-containers.sh
+++ nominatim/run-containers.sh
@@ -3,5 +3,5 @@ docker run -it \
   -e PBF_URL=https://download.geofabrik.de/europe/spain/galicia-latest.osm.pbf \
   -e REPLICATION_URL=https://download.geofabrik.de/europe/spain/galicia-updates/ \
   -p 8080:8080 \
-  --name nominatim \
+  -d --name nominatim \
   mediagis/nominatim:4.2
